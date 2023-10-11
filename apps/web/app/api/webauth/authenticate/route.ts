86dee08344058d6663e57fdffe4f5efb6c9d127a
Harsh Pareek <harshpareek91@gmail.com>
2023-10-11 23:07:41 +0530

    pleaseeee

--- apps/web/app/api/webauth/authenticate/route.ts
+++ apps/web/app/api/webauth/authenticate/route.ts
@@ -46,16 +46,6 @@ export const GET = async (request: NextRequest) => {
         } as Authenticator)
     );
 
-    const options = await generateAuthenticationOptions({
-      allowCredentials: userAuthenticators.map((authenticator) => ({
-        id: authenticator.credentialID,
-        type: "public-key",
-        // Optional
-        transports: authenticator.transports,
-        userVerification: "preferred",
-      })),
-    });
-
     // console.log("options generated...");
 
     // const userCurrentChallenge = {
