2d145cc22daa8e17ddb58b842d3b704473606ce9
Harsh Pareek <harshpareek91@gmail.com>
2023-10-12 00:41:22 +0530

    this is killing me

--- apps/web/app/api/webauth/authenticate/route.ts
+++ apps/web/app/api/webauth/authenticate/route.ts
@@ -12,9 +12,10 @@ import { Authenticator, GenerateOptions } from "@/utils/types";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
-  // get user from the clerk
+  const prisma = new PrismaClient();
 
   try {
+    // get user from the clerk
     const user = getAuth(request);
 
     if (user.userId) {
@@ -76,6 +77,8 @@ export const GET = async (request: NextRequest) => {
         status: 404,
       }
     );
+  } finally {
+    prisma.$disconnect();
   }
 };
 
