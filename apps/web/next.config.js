48db26d664552053232dcc6f30052e984493d530
Harsh Pareek <harshpareek91@gmail.com>
2023-10-12 00:49:04 +0530

    prisma errir

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,5 @@
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
+
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
@@ -9,5 +11,12 @@ module.exports = {
         permanent: true,
       },
     ];
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
   },
 };
