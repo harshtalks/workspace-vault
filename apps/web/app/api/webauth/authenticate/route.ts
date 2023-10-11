804b6592855f87a19fd5ab4da73af2377a09bae4
Harsh Pareek <harshpareek91@gmail.com>
2023-10-11 22:57:30 +0530

    cant debug

--- apps/web/app/api/webauth/authenticate/route.ts
+++ apps/web/app/api/webauth/authenticate/route.ts
@@ -15,62 +15,60 @@ export const GET = async (request: NextRequest) => {
   // get user from the clerk
 
   try {
-    const user = getAuth(request);
+    // const user = getAuth(request);
 
-    if (!user.userId) {
-      throw new Error("You are not authorized.");
-    }
+    // if (!user.userId) {
+    //   throw new Error("You are not authorized.");
+    // }
 
-    // database config
-    const prismaClient = new PrismaClient();
+    // // database config
+    // const prismaClient = new PrismaClient();
 
-    // get all the authenticators instances
-    const authResults = await prismaClient.authenticators.findMany({
-      where: {
-        userId: user.userId,
-      },
-    });
+    // // get all the authenticators instances
+    // const authResults = await prismaClient.authenticators.findMany({
+    //   where: {
+    //     userId: user.userId,
+    //   },
+    // });
 
-    console.log("Auth results found..");
+    // console.log("Auth results found..");
 
-    const userAuthenticators = authResults.map(
-      (auth) =>
-        ({
-          counter: auth.counter,
-          credentialBackedUp: auth.credentialBackedUp,
-          credentialDeviceType:
-            auth.credentialDeviceType as CredentialDeviceType,
-          credentialID: auth.credentialID,
-          credentialPublicKey: auth.credentialPublicKey,
-          transports: auth.transports as unknown as AuthenticatorTransport[],
-        } as Authenticator)
-    );
+    // const userAuthenticators = authResults.map(
+    //   (auth) =>
+    //     ({
+    //       counter: auth.counter,
+    //       credentialBackedUp: auth.credentialBackedUp,
+    //       credentialDeviceType:
+    //         auth.credentialDeviceType as CredentialDeviceType,
+    //       credentialID: auth.credentialID,
+    //       credentialPublicKey: auth.credentialPublicKey,
+    //       transports: auth.transports as unknown as AuthenticatorTransport[],
+    //     } as Authenticator)
+    // );
 
-    const options = await generateAuthenticationOptions({
-      allowCredentials: userAuthenticators.map((authenticator) => ({
-        id: authenticator.credentialID,
-        type: "public-key",
-        // Optional
-        transports: authenticator.transports,
-        userVerification: "preferred",
-      })),
-    });
+    // const options = await generateAuthenticationOptions({
+    //   allowCredentials: userAuthenticators.map((authenticator) => ({
+    //     id: authenticator.credentialID,
+    //     type: "public-key",
+    //     // Optional
+    //     transports: authenticator.transports,
+    //     userVerification: "preferred",
+    //   })),
+    // });
 
-    console.log("options generated...");
+    // console.log("options generated...");
 
-    const userCurrentChallenge = {
-      challenge: options.challenge,
-      userId: user.userId,
-    };
+    // const userCurrentChallenge = {
+    //   challenge: options.challenge,
+    //   userId: user.userId,
+    // };
 
-    prismaClient.$disconnect();
+    // prismaClient.$disconnect();
 
     return new NextResponse(
-      JSON.stringify({
-        status: "success",
-        challenge: userCurrentChallenge,
-        options: options,
-      } as GenerateOptions<PublicKeyCredentialRequestOptionsJSON>),
+      JSON.stringify(
+        {} as GenerateOptions<PublicKeyCredentialRequestOptionsJSON>
+      ),
       {
         status: 200,
       }
